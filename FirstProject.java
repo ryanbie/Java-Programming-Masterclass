f6b32c774476b805985578523b5a7ec4dce28213
Ryan Bie <30966505+ryanbie@users.noreply.github.com>
2021-10-12 12:01:04 -0400

    Update FirstProject.java
    Added an expression to print out the int variable of myFirstNumber

--- FirstProject.java
+++ FirstProject.java
@@ -3,6 +3,8 @@ public class Hello {
   public status void main(String[] args) {
     System.out.println("Hello, Ryan");
     
-    int myFirstNumber = 5;
+    int myFirstNumber = (10 + 5) + (2 * 10);
+    
+    System.out.println("Here is your value: " + myFirstNumber); 
     }
  }
