8061a4c932f447515932ee68175854163ae6e6e3
Ryan Bie <30966505+ryanbie@users.noreply.github.com>
2021-10-12 12:27:18 -0400

    Update FirstProject.java
    Added expressions

--- FirstProject.java
+++ FirstProject.java
@@ -4,7 +4,14 @@ public class Hello {
     System.out.println("Hello, Ryan");
     
     int myFirstNumber = (10 + 5) + (2 * 10);
+    int mySecondNumber = 12;
+    int myThirdNumber = myFirstNumber + 2;
+    int Results = myFirstNumber + mySecondNumber + mythirdNumber;
     
-    System.out.println("Here is your value: " + myFirstNumber); 
+    System.out.println("Here is your value: " + myFirstNumber);
+    System.out.println("Here is your values added together: " + Results);
+    
+    int myLastOne = (1000 - Results);
+    System.out.println(myLastOne); 
     }
  }
